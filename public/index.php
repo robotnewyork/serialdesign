770b426b4a4a8dd00b35a312a5eec142572fde50
robotnewyork <robotnewyork@yahoo.com>
2017-02-27 16:11:14 -0500

    Prevent cachine

--- public/index.php
+++ public/index.php
@@ -7,6 +7,13 @@
     <meta name="keywords" content="Serial Design, Boy Magician, E-Lusion">
     <meta name="author" content="Serial Design">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Prevent cachine -->
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
   </head>
 
   <body>
